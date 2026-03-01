97a5535b06e000920e60202cc61444f7826ab0b4
souparno-nag <souparnonag2004@gmail.com>
2026-03-01 12:37:26 +0530

    assigned teams to players

--- main1.py
+++ main1.py
@@ -1,6 +1,7 @@
 import cv2
 from src1.utils import read_video, save_video
 from src1.tracker import Tracker
+from src1.team_assigner import TeamAssigner
 
 def main():
     # Read Video
@@ -13,23 +14,35 @@ def main():
                                         read_from_stubs=True, 
                                         stub_path="./data/stubs/tracks_stubs.pkl")
     
-    # Save Cropped Image of a Player
-    for track_id, player in tracks["players"][0].items():
-        bbox = player["bbox"]
-        frame = video_frames[0]
-        # Crop bbox from frame
-        croped_player = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
-        # Save cropped image
-        save_path = "./data/image/cropped_img.jpg"
-        cv2.imwrite(save_path, croped_player)
-        break
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+    
+    # # Save Cropped Image of a Player
+    # for track_id, player in tracks["players"][0].items():
+    #     bbox = player["bbox"]
+    #     frame = video_frames[0]
+    #     # Crop bbox from frame
+    #     croped_player = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+    #     # Save cropped image
+    #     save_path = "./data/image/cropped_img.jpg"
+    #     cv2.imwrite(save_path, croped_player)
+    #     break
     
     # Draw Output
     # Draw Object Tracks on Frames
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
     # Save Video
-    output_video_path = "./data/output/output2.mp4"
+    output_video_path = "./data/output/output3.mp4"
     save_video(output_video_frames, output_video_path)
 
 if __name__ == "__main__":
