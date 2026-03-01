97f4547d9d0aca915b6c593ce459d78053dd751b
souparno-nag <souparnonag2004@gmail.com>
2026-03-01 11:14:56 +0530

    saving cropped image of player

--- main1.py
+++ main1.py
@@ -1,3 +1,4 @@
+import cv2
 from src1.utils import read_video, save_video
 from src1.tracker import Tracker
 
@@ -12,7 +13,19 @@ def main():
                                         read_from_stubs=True, 
                                         stub_path="./data/stubs/tracks_stubs.pkl")
     
+    # Save Cropped Image of a Player
+    for track_id, player in tracks["players"][0].items():
+        bbox = player["bbox"]
+        frame = video_frames[0]
+        # Crop bbox from frame
+        croped_player = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+        # Save cropped image
+        save_path = "./data/image/cropped_img.jpg"
+        cv2.imwrite(save_path, croped_player)
+        break
+    
     # Draw Output
+    # Draw Object Tracks on Frames
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
     # Save Video
